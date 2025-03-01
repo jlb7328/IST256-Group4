1e73b522d8182cd1fe9259c0be594df6f091fc78
Gosick3000 <vhd5011@psu.edu>
2025-02-28 21:00:22 -0500

    Update sign_up.js

--- sign_up.js
+++ sign_up.js
@@ -1,38 +1,38 @@
 // JavaScript Document
 function onClick(){
-	var name = document.getElementById("name");
-	var email = document.getElementById("email");
-	var password = document.getElementById("password");
-	var confirmpassword = document.getElementById("confirmPassword");
-	var age = document.getElementById("age");
-	var phone = document.getElementById("phone");
-	var address = document.getElementById("address");
-	
-	if (name.value == ""){
-		
-	}
-	
-	if (email.value == ""){
-		
-	}
-	
-	if (password.value == ""){
-		
-	}
-	
-	if (confirmpassword.value == ""){
-		
-	}
-	
-	if (age.value == "" && isNaN(age.value)){
-		
-	}
-	
-	if ((phone.value == "") && isNaN(phone.value)){
-		
-	}
-	
-	if (address.value == ""){
-		
-	}	
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+    var confirmpassword = document.getElementById("confirmPassword");
+    var age = document.getElementById("age");
+    var phone = document.getElementById("phone");
+    var address = document.getElementById("address");
+    
+    if (name.value == ""){
+            
+    }
+    
+    if (email.value == ""){
+            
+    }
+    
+    if (password.value == ""){
+            
+    }
+    
+    if (confirmpassword.value == ""){
+            
+    }
+    
+    if (age.value == "" && isNaN(age.value)){
+            
+    }
+    
+    if ((phone.value == "") && isNaN(phone.value)){
+            
+    }
+    
+    if (address.value == ""){
+            
+    }	
 }
