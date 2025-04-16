7a95ccd6277a43691b2bb5581e615156cda324d2
KCTops100 <kctops06@gmail.com>
2025-04-16 18:54:15 -0400

    display message if MongoDB connection is successful.

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })  // connect to MongoDB using Mongoose.
+.then(() => console.log("MongoDB connected successfully.")) // display message if MongoDB connection is successful.
 app.use(express.static(path.join(__dirname, 'public')));  // middleware to serve static files from /public
 app.use(express.json());  // middleware to parse incoming JSON data.
 app.set('view engine', 'ejs') // set the view engine to ejs.
